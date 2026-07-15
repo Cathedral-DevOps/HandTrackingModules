0c515d828135195df05c1e91b986506f22931b92
CathedralDevops <iesushominumsalvator14@gmail.com>
2026-07-14 22:27:01 -0500

    Refactor comments for clarity in htMin.py

--- htMin.py
+++ htMin.py
@@ -12,10 +12,11 @@ mpDraw = mp.solutions.drawing_utils
 
 while True:
     success, img = cap.read()
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
-    print(results.multi_hand_landmarks)
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #convert to rgb for process
+    results = hands.process(imgRGB) #process
+    print(results.multi_hand_landmarks) #print detected
     
+    #nested if loop for drawing points on hands
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
             mpDraw.draw_landmarks(img, handLms)
